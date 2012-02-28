d4019e9ef1167b6a7aae253538af56f2f12d60d9
gareeblackwood@gmail.com <gareeblackwood@gmail.com@2a5da9f5-19d7-c2bd-6a33-479c075cc0d5>
2012-02-28 15:39:38 +0000

    Initial project commit.

---
+++ trunk/app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Incident( models.Model ):
+    title = models.CharField( max_length = 200 )
+    description = models.TextField()
+    submission_time = models.DateTimeField( 'Submission Time' )
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+
+    def __unicode__( self ):
+        return self.title
+
+    def incVote( self ):
+        self.upvotes += 1
+
+    def decVote( self ):
+        self.downvotes += 1
+
+    def netVote( self ):
+        return self.upvotes - self.downvotes
+
+    def was_submitted_today( self ):
+        return self.submission_time.date() == datetime.date.today()
+
+    
+
+    
