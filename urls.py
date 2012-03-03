d957a134ee428695778b7a2442d36d73efc1f8c3
gareeblackwood <gareeblackwood@gmail.com>
2012-03-03 15:11:29 +0000

    It will work now.

--- urls.py
+++ urls.py
@@ -6,6 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url( r'^app/$', 'app.views.index' ),
+                       url( r'^app/submit$', 'app.views.submit' ),
+                       url( r'^app/help$', 'app.views.help' ),
+                       url( r'^app/about$', 'app.views.about' ),
+                       url( r'^app/contact$', 'app.views.contact' ),
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 )
